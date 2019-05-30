7928d752b0f5f7f337cf1c52367ed2ee7bcb0a0a
Gokberk Yaltirakli <webdosusb@gmail.com>
2019-05-30 21:57:55 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# lobsters client library
+
+This library can be used to retrieve data from Lobste.rs.
+
+# Tests
+
+The tests can be ran by going to the root of the repository and running the
+`prove` command.
+
+# Contributing
+
+Before sending a PR, make sure that all the tests are passing. If you have
+added or altered any functionality, you should write new tests to cover them
+and make sure that the tests reflect the correct behaviour.
+
+Patches can be sent by Pull Requests or via email to `lobsters-py at gkbrk dot
+com`.
---
+++ lobsters.py
@@ -0,0 +1,80 @@
+import json
+import requests
+import typing
+
+class User:
+    about: str
+    avatar_url: str
+    created_at: str
+    github_username: str
+    invited_by_user: str
+    is_admin: bool
+    is_moderator: bool
+    karma: int
+    twitter_username: str
+    username: str
+
+    def from_dict(data):
+        user = User()
+
+        user.about = data.get('about', '')
+        user.avatar_url = data.get('avatar_url', '')
+        user.created_at = data.get('created_at', '')
+        user.github_username = data.get('github_username', '')
+        user.invited_by_user = data.get('invited_by_user', '')
+        user.is_admin = data.get('is_admin', False)
+        user.is_moderator = data.get('is_moderator', False)
+        user.karma = data.get('karma', 0)
+        user.twitter_username = data.get('twitter_username', '')
+        user.username = data.get('username', '')
+
+        return user
+
+
+class Story:
+    comment_count: int
+    created_at: str
+    downvotes: int
+    score: int
+    short_id: str
+    short_id_url: str
+    title: str
+    upvotes: int
+    url: str
+    user: User
+    description: str
+
+    def from_dict(data):
+        story = Story()
+
+        story.created_at = data.get('created_at', '')
+        story.short_id = data.get('short_id', '')
+        story.title = data.get('title', '')
+        story.url = data.get('url', '')
+        story.user = User.from_dict(data.get('submitter_user', {}))
+        story.description = data.get('description', '')
+
+        return story
+
+class Client:
+    def __init__(self, base='https://lobste.rs'):
+        self.__base = base
+        self.__s = requests.Session()
+
+    def frontpage(self):
+        url = self._url('/hottest.json')
+        data = self._get_json(url)
+
+        return map(Story.from_dict, data)
+
+    def get_user(self, username) -> User:
+        url = self._url(f'/u/{username}.json')
+        data = self._get_json(url)
+
+        return User.from_dict(data)
+
+    def _get_json(self, url):
+        return self.__s.get(url).json()
+
+    def _url(self, path):
+        return f'{self.__base}{path}'
