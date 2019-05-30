5861a23b278b623c6e8f56d14cd84e14d67833ce
Gokberk Yaltirakli <webdosusb@gmail.com>
2019-05-30 22:21:27 +0100

    Added setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name = "lobsters",
+    version = "0.2.0",
+    packages = find_packages(),
+    install_requires = ['requests>=2.0', 'testsimple>=0.1'],
+    url = "https://github.com/gkbrk/lobsters-py",
+    author = "Gokberk Yaltirakli",
+    author_email = "opensource@gkbrk.com",
+    description = "API Client for lobste.rs",
+    keywords = "api client lobsters lobste",
+    project_urls = {
+        'Bug Tracker': 'https://github.com/gkbrk/lobsters-py/issues',
+        'Source Code': 'https://github.com/gkbrk/lobsters-py'
+    }
+)
